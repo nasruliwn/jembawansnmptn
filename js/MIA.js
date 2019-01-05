73cd739e1edfc1831292d0d641d03c994c24b0f1
Rafid Aslam <rafidteam@gmail.com>
2019-01-05 21:10:29 +0700

    IIS_A.xlsx: Add Wisanggalih's and Veronica's data

--- js/MIA.js
+++ js/MIA.js
@@ -10,5 +10,5 @@ $(document).ready(function () {
 
     $('.progress-bar').css('width', dataPercentage + '%').attr('aria-valuenow', dataPercentage);
     $('.progress-bar').text(`Data terkumpul ${DATA_COLLECTED} dari ${DATA_EXPECTED}`);
-    $('#lastUpdate').text('Terakhir diupdate: Jumat,  4 Jan 2019 11:24:06');
+    $('#lastUpdate').text('Terakhir diupdate: Sabtu,  5 Jan 2019 21:08:36');
 });
