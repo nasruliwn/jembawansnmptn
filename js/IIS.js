1a5aabcfe1da6b2b3a2aa0fd55d2577dbb4e096e
Travis CI User <travis@example.org>
2019-02-04 07:13:00 +0000

    [travis] Update data

--- js/IIS.js
+++ js/IIS.js
@@ -10,6 +10,6 @@ $(document).ready(function () {
 
     $('.progress-bar').css('width', dataPercentage + '%').attr('aria-valuenow', dataPercentage);
     $('.progress-bar').text(`Data terkumpul ${DATA_COLLECTED} dari ${DATA_EXPECTED}`);
-    $('#lastUpdate').text('Last checked: Sunday,  3 Feb 2019 14:12:28 (WIB)');
+    $('#lastUpdate').text('Last checked: Monday,  4 Feb 2019 14:12:57 (WIB)');
     $('#downloadURL').html('Data beserta nilai bisa diunduh di <a href="data/jathayu-iis-rank.csv">sini</a> (IIS)');
 });
