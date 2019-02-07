7037b0080c53c3ab8a779fe4486f0e7fe27bf786
Travis CI User <travis@example.org>
2019-02-07 04:20:07 +0000

    [travis] Update data

--- js/MIA.js
+++ js/MIA.js
@@ -10,6 +10,6 @@ $(document).ready(function () {
 
     $('.progress-bar').css('width', dataPercentage + '%').attr('aria-valuenow', dataPercentage);
     $('.progress-bar').text(`Data terkumpul ${DATA_COLLECTED} dari ${DATA_EXPECTED}`);
-    $('#lastUpdate').text('Last checked: Thursday,  7 Feb 2019 11:13:46 (WIB)');
+    $('#lastUpdate').text('Last checked: Thursday,  7 Feb 2019 11:20:02 (WIB)');
     $('#downloadURL').html('Data beserta nilai bisa diunduh di <a href="data/jathayu-mia-rank.csv">sini</a> (MIA)');
 });
